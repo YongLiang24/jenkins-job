91dec2fe8d84aa337327ed830bb03ccf364eb7a0
‘yongliang24’ <yongliang24@gmail.com>
2020-03-11 13:30:33 -0400

    first commit

---
+++ JenkinsJ/src/main/java/MathOperation.java
@@ -0,0 +1,15 @@
+
+public class MathOperation {
+	public static int add(int i, int j) {
+		return i+j;
+	}
+	public static int subtract(int i, int j) {
+		return i - j;
+	}
+	public static int multiply(int i, int j) {
+		return i - j;
+	}
+	public static int divide(int i, int j) {
+		return i / j;
+	}
+}
---
+++ JenkinsJ/src/test/java/MathOperateTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+public class MathOperateTest {
+
+	int x,y;
+	   @BeforeEach
+	  public void setUp() throws Exception { 
+		  x=4;y=5;
+		  System.out.println("before");
+	  }
+	@DisplayName("Testing the Add Method...")
+	@Test
+	public void testAdd() {
+		System.out.println("Testing add . . . ");
+		int result = MathOperation.add(x,y);
+		System.out.println(result);
+		assertTrue(result == 15);
+	}
+	@DisplayName("Testing the Sub Method...")
+	@Test
+	public void testSub() {
+		System.out.println("Testing Sub . . . ");
+		int result = MathOperation.subtract(x,y);
+		System.out.println(result);
+		assertTrue(result == -1);
+	}
+
+}
